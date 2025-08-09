365209d1b3d282c2fece4c5369ded497c524062a
Dmitry Bogdanov <dmtribogdanov@gmail.com>
2025-08-09 07:48:24 +0500

    lesson-04, task 1-4

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,4 +12,10 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(str) {
+  let doubleStr = '';
+  for (let i = 0; i < str.length; i++) {
+    doubleStr += str[i] + str[i];
+  }
+  return doubleStr;
+}
