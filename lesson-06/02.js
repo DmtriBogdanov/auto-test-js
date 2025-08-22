6948ca2fef57f6b0622d2a11a44c1f53d413aded
Dmitry Bogdanov <dmtribogdanov@gmail.com>
2025-08-22 13:43:34 +0500

    lesson-06, task-02

--- lesson-06/02.js
+++ lesson-06/02.js
@@ -24,4 +24,13 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+const map = (array, callback) => {
+  const newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    if (callback(element, index)) {
+      newArr.push(element);
+    }
+  }
+  return newArr;
+}
